57039c8168059d4d9690780c3208a1da9879a4be
Fernando Cordisco <fernandorcvilela@hotmail.com>
2023-01-14 12:07:39 -0300

    Colisão e mapa

--- src/Game.java
+++ src/Game.java
@@ -1,19 +1,26 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
 
-public class Game extends Canvas implements Runnable {
+public class Game extends Canvas implements Runnable, KeyListener {
 
     public static int WIDTH = 480, HEIGHT = 480;
+    public Player player;
+    public World world;
 
     public Game(){
+        this.addKeyListener(this);
         this.setPreferredSize(new Dimension(WIDTH   ,HEIGHT));
+        player = new Player(32,32 );
+        world = new World();
 
     }
 
     public void tick(){
-
+        player.tick();
     }
 
     public void render(){
@@ -30,8 +37,8 @@ public class Game extends Canvas implements Runnable {
         g.setColor(Color.black);
         g.fillRect(0,0,WIDTH,HEIGHT);
 
-        g.setColor(Color.red);
-        g.fillRect(10,10,100,100);
+        player.render(g);
+        world.render(g);
 
         bs.show();
 
@@ -65,5 +72,44 @@ public class Game extends Canvas implements Runnable {
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+            player.right = true;
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_LEFT){
+            player.left = true;
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_UP){
+            player.up = true;
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_DOWN){
+            player.down = true;
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+            player.right = false;
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_LEFT){
+            player.left = false;
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_UP){
+            player.up = false;
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_DOWN){
+            player.down = false;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
